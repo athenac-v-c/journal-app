4961dd0ad2bc3c514faf0642c8feb3fa24f0c744
athenac-v-c <aprilyang065@gmail.com>
2025-10-11 15:29:02 -0400

    [UPDATE] editor UI

--- components/ui/rich-text-editor/menu-bar.tsx
+++ components/ui/rich-text-editor/menu-bar.tsx
@@ -1,13 +1,101 @@
 'use client'
+import { Toggle } from "../toggle"
+import { AlignCenter, AlignLeft, AlignRight, Bold, Heading1, Heading2, Heading3, Highlighter, Italic, List, ListOrdered, Strikethrough, TextAlignCenter } from "lucide-react"
 import React from "react"
-im
-export default function MenuBar({editor}){
+import { Editor } from "@tiptap/react"
+
+export default function MenuBar({editor}:{editor:Editor | null}){
      
   if (!editor) {
     return null
   }
+  const options = [
+    {
+      icon: <Heading1 className="size-4" />,
+      onClick:()=>editor.chain().focus().toggleHeading({level:1}).run(),
+      pressed:editor.isActive('heading',{level:1}),
+    },
 
-  return(
+    {
+      icon: <Heading2 className="size-4" />,
+      onClick:()=>editor.chain().focus().toggleHeading({level:1}).run(),
+      pressed:editor.isActive('heading',{level:2}),
+    },
+
+    {
+     icon: <Heading3 className="size-4" />,
+      onClick:()=>editor.chain().focus().toggleHeading({level:1}).run(),
+      pressed:editor.isActive('heading',{level:1}),
+    },
+    {
+     icon: <Bold className="size-4" />,
+     onClick:()=>editor.chain().focus().toggleBold().run(),
+     pressed:editor.isActive('bold'),
+
+    },
+    {
+     icon: <Italic className="size-4" />,
+     onClick:()=>editor.chain().focus().toggleItalic().run(),
+     pressed:editor.isActive('italic'),
+
+    },
+    {
+     icon: <Strikethrough  className="size-4" />,
+     onClick:()=>editor.chain().focus().toggleStrike().run(),
+     pressed:editor.isActive('strike'),
+
+    },
+    {
+     icon: <Highlighter  className="size-4" />,
+     onClick:()=>editor.chain().focus().toggleHighlight().run(),
+     pressed:editor.isActive('highlight'),
+
+    },
+    {
+     icon: <AlignCenter  className="size-4" />,
+     onClick:()=>editor.chain().focus().setTextAlign('center').run(),
+     pressed:editor.isActive({textAlign:'center'}),
+
+    },
+    {
+      icon: <AlignLeft  className="size-4" />,
+     onClick:()=>editor.chain().focus().setTextAlign('left').run(),
+     pressed:editor.isActive({textAlign:'left'}),
+
+    },
+    {
+     icon: <AlignRight className="size-4" />,
+     onClick:()=>editor.chain().focus().setTextAlign('right').run(),
+     pressed:editor.isActive({textAlign:'right'}),
+
+    },
+    {
+     icon: <List  className="size-4" />,
+     onClick:()=>editor.chain().focus().toggleBulletList().run(),
+     pressed:editor.isActive('bulletList'),
+
+    },
+    {
+     icon: <ListOrdered  className="size-4" />,
+     onClick:()=>editor.chain().focus().toggleOrderedList().run(),
+     pressed:editor.isActive('orderedList'),
+
+    },
+  ];
+  
+   return (
+     <div className="border rounded-md p-1 mb-1 bg-slate-50 space-x-2 z-50">
+       {options.map((option,index)=>(
+
+         <Toggle key={index} pressed ={option.pressed} onPressedChange={option.onClick}>{option.icon}</Toggle>
+      ))}
+     </div>
+
+    );
+
+}
+
+/**return(
   
         <div className="control-group">
           <div className="button-group">
@@ -77,20 +165,5 @@ export default function MenuBar({editor}){
         </div>
         
    
-  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+  )**/
+   
