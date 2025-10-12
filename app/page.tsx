b732c31c12b838eb85dd7806492ee7fb3b870d3d
athenac-v-c <aprilyang065@gmail.com>
2025-10-11 22:38:27 -0400

    [ADD] get post value

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+import { useState } from "react";
 import Image from "next/image";
 import RichTextEditor from "@/components/ui/rich-text-editor";
+
 export default function Home() {
+  const [post,setPost] =useState('')
+  const onChange=(content)=>{
+    //setPost(e.target.value);
+    setPost(content)
+    console.log(content)
+  }
   return (
     
-    <div className="max-w-5xl mx-auto py-8 ">
-     <RichTextEditor/>
+    <div className="max-w-5xl max-w-5xl mx-auto py-8 ">
+     <RichTextEditor content={post} onChange={onChange}/>
     </div>
     
   
--- components/ui/rich-text-editor/index.tsx
+++ components/ui/rich-text-editor/index.tsx
@@ -1,60 +1,62 @@
 'use client'
-import { useEditor, EditorContent} from "@tiptap/react";
+import { useEditor, EditorContent, Editor} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import React from "react";
 import MenuBar from "./menu-bar";
 import TextAlign from "@tiptap/extension-text-align";
 import Highlight from '@tiptap/extension-highlight'
 
-const RichTextEditor=()=>{
-  const editor = useEditor({
-    extensions:[
+interface RichTextEditorPors{
+    content: string
+    onChange:(content:string) =>void;
 
-        StarterKit.configure({
-          bulletList:{
-            HTMLAttributes: {
-            class: 'list-disc ml-4',
-          },
-        },
-          orderedList:{
-            HTMLAttributes: {
-            class: 'list-decimal ml-4',
-          },
+}
+
+const RichTextEditor=({content,onChange}:RichTextEditorPors)=>{
+    const editor = useEditor({
+        extensions:[
+
+            StarterKit.configure({
+                bulletList:{
+                    HTMLAttributes: {
+                    class: 'list-disc ml-4',
+                },
+            },
+                orderedList:{
+                    HTMLAttributes: {
+                    class: 'list-decimal ml-4',
+                },
           
-         },
-  }),
-        TextAlign.configure({
-            types: ['heading', 'paragraph'],
-            alignments: ['left', 'right','center'],
-        }),
-  
-        Highlight.configure({
-            HTMLAttributes: {
-            class: 'hover:bg-red-500',
-             },
-             multicolor: true,
-        })
-        
-    ],
-    content:'<p> please edit </p>',
-    immediatelyRender:false,
-    editorProps:{
-        attributes:{
-            class:'min-h-[156px] border rounded-md bg-slate-50 py-2 px-3'
+            },
+
+            }),
+            TextAlign.configure({
+                types: ['heading', 'paragraph'],
+                alignments: ['left', 'right','center'],
+            }),
+            Highlight, 
+        ],
+        content:content,
+        immediatelyRender:false,
+        editorProps:{
+            attributes:{
+                class:'min-h-[156px] border rounded-md bg-slate-50 py-2 px-3'
+            },
         },
-    },
 
-  },
-   
+        onUpdate:({editor}) =>{
+            console.log(editor.getHTML())
+            onChange(editor.getHTML())
+        }
 
+    });
 
-  )
-  return (
-  <div>
-  <MenuBar editor={editor}/>
-  <EditorContent editor={editor}/>
-  </div>
-  )
+    return (
+        <div>
+        <MenuBar editor={editor}/>
+        <EditorContent editor={editor}/>
+        </div>
+    )
 
 }
 export default RichTextEditor
