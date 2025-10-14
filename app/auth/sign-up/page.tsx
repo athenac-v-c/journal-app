1ce0338a12957e8af61d25048f6f2e8bc5fc4e67
athenac-v-c <aprilyang065@gmail.com>
2025-10-13 23:37:53 -0400

    fix(auth): correct state handler for password input

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -64,7 +64,7 @@ export default function SignUp(){
                     type='password' 
                     name='password'
                     value={password}
-                    onChange={(e)=>setUsername(e.target.value)}
+                    onChange={(e)=>setPassword(e.target.value)}
                 />
             </div>
             </div>
