da4bbdd46edb155f11e93961856932ecc7f63a63
athenac-v-c <aprilyang065@gmail.com>
2025-10-13 23:33:47 -0400

    update(auth):refine sign-up page logic and form behavior

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -2,21 +2,31 @@
 import "../page.css"
 import { useState } from "react"
 
+type User={
+       username:string;
+       password:string;
+    }
 
 export default function SignUp(){
-    type User={
-       username:string,
-       password:string
-    }
-    const [User,setUser] = useState<User|null>(null)
-    const getUser = () =>{
-       
+    
+    //const [User,setUser] = useState<User|null>(null)
+    
+    const [username, setUsername] = useState('')
+    const [password,setPassword] = useState('')
+    
+    const handleSignUp = (e:React.FormEvent)=>{
+      
+        e.preventDefault();// form submission will reload the page since it sends a request and clears everything, this prevents reloading behavior
+        
+        //get the username and password from submitted form and store it into new user object
+        const newUser: User = {username, password}
 
+        console.log('New user created:', newUser)
 
     }
 
     return(
-     <>
+     <form className="form" onSubmit={handleSignUp}>
         <div className="form">
             <div>
                 <h1>Create a new account</h1>
@@ -35,6 +45,8 @@ export default function SignUp(){
                     id='username' 
                     type='text' 
                     name='username'
+                    value={username}
+                    onChange={(e)=>setUsername(e.target.value)}
                 />
             </div>
             </div>
@@ -51,16 +63,19 @@ export default function SignUp(){
                     id='password' 
                     type='password' 
                     name='password'
+                    value={password}
+                    onChange={(e)=>setUsername(e.target.value)}
                 />
             </div>
             </div>
             <div >
-                <button id='btn' >
+                <button id='btn' type="submit">
                     sign up
                 </button>
             </div> 
+            
         </div>
-    </>
+    </form>
 
     )
 
