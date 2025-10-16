28dbb0eb4a9b21e079f39d26044c29686380c070
athenac-v-c <aprilyang065@gmail.com>
2025-10-15 22:13:33 -0400

    feat(api): create /api/notes route to save and fetch notes

---
+++ app/api/notes/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+
+let mockDB: { id: string; content: string }[] = []
+
+export async function POST(req: Request) {
+  const { content } = await req.json()
+  const newPost = { id: Date.now().toString(), content }
+  mockDB.push(newPost)
+  console.log('get a new note')
+  return NextResponse.json({ message: 'saved', post: newPost })
+}
+
+export async function GET() {
+  return NextResponse.json(mockDB)
+}
