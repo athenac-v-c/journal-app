a4f04782acd5eae9beb503409c3c380ca5b95e87
athenac-v-c <aprilyang065@gmail.com>
2025-10-22 13:22:34 -0400

    fix(shownote):add placeholder content to fix Vercel module issue

---
+++ app/showNote/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+
+export default function ShowNote() {
+  return (
+    <div className="show-note-placeholder">
+      <h2>Show Note</h2>
+      <p>This page is under development.</p>
+    </div>
+  )
+}
