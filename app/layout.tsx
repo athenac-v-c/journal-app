12e57c7eeae947491c69bcc2001890026b18aa4f
athenac-v-c <aprilyang065@gmail.com>
2025-10-22 14:38:24 -0400

    fixt(layout):add proper viewport meta tag to the head

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'></link>
+        <meta name="viewport" content="width=device-width,initial-scale=1..0"></meta>
       </head>
       <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased home-page`}
