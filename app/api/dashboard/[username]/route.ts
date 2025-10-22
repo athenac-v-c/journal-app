1ed8ea0da91e98a047065fe2d0f23831b63003c0
athenac-v-c <aprilyang065@gmail.com>
2025-10-22 12:53:45 -0400

    feat(dashboard): add dynamic [username] route for fetching user dashboard data

---
+++ app/api/dashboard/[username]/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import {Redis} from "@upstash/redis"
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_URL!,
+    token:process.env.UPSTASH_REDIS_TOKEN!,
+})
+interface Dashboard{
+    ownerEmail:string,
+    noteIds: string[]
+}
+export async function GET(req:Request,{params}:{params:{username:string}}) {
+
+    const {searchParams} = new URL(req.url)
+    const email = searchParams.get("email")
+
+    if(!email){
+        return NextResponse.json({success:false, message:"Email missing"})
+    }
+
+    const dashboards = JSON.parse((await redis.get("dashboards")) || "[]")
+    const dashboard = dashboards.find((d:Dashboard) => d.ownerEmail === email)
+
+
+    if(!dashboard){
+
+        return NextResponse.json(
+            {success:false, message:`${email}'s dashboard not found`}
+        )
+    }
+    return NextResponse.json({
+        success:true,
+        username:params.username,
+        noteIds:dashboard.noteIds
+    })   
+}
