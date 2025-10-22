d43601d451f8a6d873c6355dbf19f90c727e767b
athenac-v-c <aprilyang065@gmail.com>
2025-10-22 13:57:53 -0400

    fix(api/dashboard): add : Promise<Response> to force return

--- app/api/dashboard/[username]/route.ts
+++ app/api/dashboard/[username]/route.ts
@@ -10,7 +10,7 @@ interface Dashboard{
     ownerEmail:string;
     noteIds: string[];
 }
-export async function GET(req:NextRequest,context:{ params:{ username:string} }) {
+export async function GET(req:NextRequest,context:{ params:{ username:string} }): Promise<Response> {
 
 
     const {username} = context.params
